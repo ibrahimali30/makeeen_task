f3f90a5db4e2203cec326ba76dd5b397fa7b0dd0
ibrahim <e.ali@tooli.tv>
2021-07-12 20:54:58 +0200

    create git repo and add main dependencies

---
+++ app/src/main/java/com/ahmed/makeeen_task/base/AppAplication.kt
@@ -0,0 +1,13 @@
+package com.ibrahim.extremesolutionstask.base
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import dagger.Lazy
+import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
+
+@HiltAndroidApp
+open class AppAplication : Application() {
+
+}
---
+++ app/src/main/java/com/ahmed/makeeen_task/base/AppConstants.kt
@@ -0,0 +1,5 @@
+package com.ahmed.makeeen_task.base
+
+
+const val BASE_URL = "https://apis.makeeen.com/api/provider/"
+
